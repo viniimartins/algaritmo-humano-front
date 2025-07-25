6736d3a4aa9a7b969d918e2788b679a6be6e3544
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2025-07-25 16:11:23 -0300

    feat: env example

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
       <div className="mx-auto max-w-md text-center">
         <div className="text-primary mx-auto h-12 w-12" />
         <h1 className="text-foreground mt-4 text-3xl font-bold tracking-tight sm:text-4xl">
-          Ops, página não encontrada!!
+          Ops, página não encontrada!
         </h1>
         <p className="text-muted-foreground mt-4">
           A página que você está procurando não existe ou foi movida.
